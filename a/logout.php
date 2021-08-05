c26de108e664e35e7075a314170ecd898900a3d8
ligrevx <ligrevx@gmail.com>
2021-08-05 15:47:45 +0800

    .

--- a/logout.php
+++ a/logout.php
@@ -1,8 +1,5 @@
 <?php
 session_start();
-
-if( session_status() === PHP_SESSION_ACTIVE ) {
- session_destroy();
- header('location: /a/index.html');
- exit;
-}
+session_destroy();
+header('location: /a/index.html');
+exit;
